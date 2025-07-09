e9cab297e7a7800707b0eda6762ac86c5e0bc377
João Gabriel Lenovo <joaogabriel9633@gmail.com>
2025-07-09 18:33:10 -0300

    Update page accommodation

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,18 +21,18 @@ const buttonVariants = cva(
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
         primary:
-          "bg-[#272727] uppercase rounded-none px-10 tracking-[0.2rem] font-light hover:bg-[#272727]/90",
+          "bg-[#272727] uppercase rounded-none px-10 tracking-[0.2rem] font-light hover:bg-[#272727]/90 text-white",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        lg: "h-10  px-6 has-[>svg]:px-4",
         icon: "size-9",
       },
     },
     defaultVariants: {
       variant: "default",
-      size: "default",
+      size: "lg",
     },
   }
 );
--- src/templates/accommodation/sections/accommodation-section/accommodation-section.tsx
+++ src/templates/accommodation/sections/accommodation-section/accommodation-section.tsx
@@ -1,18 +1,42 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export function AccommodationSection() {
   return (
-    <section className="min-h-screen py-8">
-      <div className="w-full max-w-7xl mx-auto flex flex-col">
+    <section className="min-h-screen py-8 px-4">
+      <div className="w-full max-w-7xl mx-auto flex flex-col items-start">
         {/* Card Left */}
-        <div>
-          <div className="">
+        <div className="flex items-center gap-16">
+          <div className="overflow-hidden w-full h-64">
             <Image
               src="/image-5.jpg"
               alt="Imagem"
-              fill
-              className="object-cover"
+              width={500}
+              height={500}
+              className="object-cover w-full h-full"
             />
+          </div>
+          <div className="flex flex-col items-start">
+            <div>
+              <h3 className="uppercase">Select</h3>
+              <p>
+                Localizados no térreo e no 1º piso, área total de 23 m²,
+                distribuídos entre varanda privativa, quarto e banheiro.
+                Acomodam até 02 pessoas em cama de casal (King Size). Possui ar
+                condicionado split, 01 TV Smart 40”, acesso gratuito ao wi-fi,
+                mesa de trabalho, frigobar, cofre eletrônico, e apoio de
+                cabeceira. Espelho de corpo inteiro, armário com cabides e
+                portas, ferro e mesa de passar roupa, secador de cabelo e
+                blackout nas cortinas.
+              </p>
+            </div>
+
+            <div className="flex items-center justify-between w-full">
+              <Button variant="primary" className="uppercase">
+                Reserve agora
+              </Button>
+              <span>Saiba mais</span>
+            </div>
           </div>
         </div>
       </div>
